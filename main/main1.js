6ee240f3347efd962f2091f0631f2789f6d12601
Ganesh Dutt <gdutt2304@gmail.com>
2024-04-15 19:33:38 +0530

    add js class

---
+++ main/main1.js
@@ -0,0 +1,41 @@
+
+//  for in loop 
+// let a ={
+//     name:"Yash_bhai ",
+//     age:45,
+//     color:"white",
+//     study:"Javascript"
+
+// }
+// // document.writeln(a.name + " : "+  a.age);
+// for( let shiv in a){
+// document.writeln(  shiv+ ": " +  a[shiv] + '<br>' );
+
+// }
+
+
+// for of loop 
+
+// let a = [12,23,4556,"Shivang" , "Aman" , "Yash bhai"]
+
+// for(let x of a){
+//     document.writeln(x);
+    
+// }
+
+// document.writeln(a);
+
+
+
+// nested loop
+
+ for(let a =1 ; a <= 4; a++){
+    for(let b =1; b <= 4; b++){
+        document.writeln(a + " : "+ b + "<br>");
+        
+    }
+ }
+
+
+
+
