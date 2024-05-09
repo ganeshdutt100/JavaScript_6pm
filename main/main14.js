def0fa19ddb5046488b2a1c13d06fb1cc886b714
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-09 19:12:17 +0530

    Update array

--- main/main14.js
+++ main/main14.js
@@ -9,3 +9,25 @@ for(x of fruits){
 }
 
 // document.writeln(fruits + '<br>' );
+let fruits1 = ['apple', 'banana'];
+ let fruits2 = ['orange', 'kiwi'];
+ 
+//  let combinedFruits = fruits1 +" " + fruits2;
+ let combinedFruits = fruits1.concat(fruits2);
+
+console.log(combinedFruits);
+
+document.writeln(combinedFruits + '<br>' );
+
+ // Result: ['apple', 'banana', 'orange', 'kiwi'
+
+
+ let arr1 = ['shivang','aman','yash','deepak','sun']
+//  let arr1output = arr1.includes("shivang")
+//  let arr1output = arr1.indexOf("shivang")
+//  let arr1output = arr1.join(" , ")
+//  let arr1output = arr1.slice(2,4)
+ let arr1output = arr1.toString()
+console.log(arr1output);
+
+ 
