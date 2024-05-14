be31f7435003f8bcd5d7a5b06700a092ada4e0a0
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-14 17:37:27 +0530

    Array methods

---
+++ main/main16.js
@@ -0,0 +1,19 @@
+document.writeln("class 16");
+
+let a = {
+    length:4,
+    1:'Yash',
+    2:'Aaman',
+    3:'Dipak',
+    0:'shivang'
+}
+
+let  output = Array.from(a);
+console.log(output);
+
+
+// of 
+// let arr = [ 'Yash', 'Aaman', 'Dipak']
+let arr = Array.of('Yash', 'Aman', 'Dipak','Shivang');
+
+console.log(arr);
