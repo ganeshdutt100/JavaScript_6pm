0416392774f1e92de98f623e3a5a4b9507131a7e
Ganesh Dutt <gdutt2304@gmail.com>
2024-05-22 16:08:21 +0530

    Events

--- main/main17.js
+++ main/main17.js
@@ -1,7 +1,3 @@
 document.writeln("Class 17");
-let colorBox =  document.getElementById('box')
-
-colorBox.addEventListener('click' , function(){
-    this.style.backgroundColor = 'green'
-});
+let colorBox =  document.getElementById
 
